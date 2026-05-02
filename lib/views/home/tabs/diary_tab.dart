2ecbbcec5a80f9bba925a9f4c46bf4b014e16c84
hoangadrop4-blip <hoangadrop4@gmail.com>
2026-05-02 09:55:15 +0700

    Auto-sync and push: 02/05/2026  9:55:15,79

--- lib/views/home/tabs/diary_tab.dart
+++ lib/views/home/tabs/diary_tab.dart
@@ -761,17 +761,20 @@ class _DiaryTabState extends State<DiaryTab> {
                           maxWidth: 900,
                         );
                         return InteractiveViewer(
-                          child: Image(
-                            image: imageProvider,
-                            fit: BoxFit.contain,
-                            width: double.infinity,
-                            height: double.infinity,
-                            filterQuality: FilterQuality.medium,
-                            gaplessPlayback: true,
-                            errorBuilder: (context, error, stackTrace) =>
-                                const Icon(
-                              Icons.broken_image,
-                              color: Colors.grey,
+                          child: Hero(
+                            tag: 'memory_image_${initialItem['id']}',
+                            child: Image(
+                              image: imageProvider,
+                              fit: BoxFit.contain,
+                              width: double.infinity,
+                              height: double.infinity,
+                              filterQuality: FilterQuality.medium,
+                              gaplessPlayback: true,
+                              errorBuilder: (context, error, stackTrace) =>
+                                  const Icon(
+                                Icons.broken_image,
+                                color: Colors.grey,
+                              ),
                             ),
                           ),
                         );
