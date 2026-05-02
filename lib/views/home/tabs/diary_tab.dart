56055f0d4b2b3bba59e521b50753281e56c2d63b
hoangadrop4-blip <hoangadrop4@gmail.com>
2026-05-02 08:45:51 +0700

    Auto-sync and push: 02/05/2026  8:45:51,49

--- lib/views/home/tabs/diary_tab.dart
+++ lib/views/home/tabs/diary_tab.dart
@@ -354,7 +354,8 @@ class _DiaryTabState extends State<DiaryTab> {
                 if (option != options.last)
                   const Divider(height: 1, color: Color(0xFFE5EAF1)),
               ],
-            ],
+              ],
+            ),
           ),
         ),
       ),
