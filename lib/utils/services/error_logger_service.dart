792c904e9a405aae40eaf2648322f4ce0698b6ab
hoangadrop4-blip <hoangadrop4@gmail.com>
2026-05-02 09:46:11 +0700

    Auto-sync and push: 02/05/2026  9:46:11,90

---
+++ lib/utils/services/error_logger_service.dart
@@ -0,0 +1,51 @@
+import 'dart:async';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
+
+class ErrorLoggerService {
+  static final ErrorLoggerService instance = ErrorLoggerService._internal();
+  
+  ErrorLoggerService._internal();
+
+  Future<void> initialize() async {
+    // Pass all uncaught "fatal" errors from the framework to Crashlytics
+    FlutterError.onError = (errorDetails) {
+      FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+    };
+    
+    // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+    PlatformDispatcher.instance.onError = (error, stack) {
+      FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+      return true;
+    };
+    
+    if (kDebugMode) {
+      // Force disable Crashlytics collection while in debug mode
+      await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
+    } else {
+      await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+    }
+  }
+
+  Future<void> logError(dynamic error, StackTrace? stack, {String? reason, bool fatal = false}) async {
+    debugPrint('Logging error: $error');
+    await FirebaseCrashlytics.instance.recordError(
+      error,
+      stack,
+      reason: reason,
+      fatal: fatal,
+    );
+  }
+
+  Future<void> setUserId(String userId) async {
+    await FirebaseCrashlytics.instance.setUserId(userId);
+  }
+
+  Future<void> log(String message) async {
+    await FirebaseCrashlytics.instance.log(message);
+  }
+
+  Future<void> setCustomKey(String key, dynamic value) async {
+    await FirebaseCrashlytics.instance.setCustomKey(key, value);
+  }
+}
