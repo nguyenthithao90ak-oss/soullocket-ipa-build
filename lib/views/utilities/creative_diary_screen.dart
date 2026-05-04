25e98590b12db2a609918888199e82e20a7e1650
hoangadrop4-blip <hoangadrop4@gmail.com>
2026-05-05 06:21:28 +0700

    Sync latest IPA source

--- lib/views/utilities/creative_diary_screen.dart
+++ lib/views/utilities/creative_diary_screen.dart
@@ -402,7 +402,7 @@ class _CreativeDiaryScreenState extends State<CreativeDiaryScreen> {
                           )
                         : const SizedBox.shrink(),
                   ),
-                  if (hasPages)
+                  if (false && hasPages)
                     Padding(
                       padding: EdgeInsets.fromLTRB(
                         compact ? 14 : 16,
