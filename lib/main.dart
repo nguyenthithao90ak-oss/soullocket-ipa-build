9998c1a59fd9ef5ef1a87962043e1d554cf2ae49
hoangadrop4-blip <hoangadrop4@gmail.com>
2026-05-05 14:00:09 +0700

    Sync latest IPA source

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:app_tracking_transparency/app_tracking_transparency.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
