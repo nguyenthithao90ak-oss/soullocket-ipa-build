beafc8fc53c3bde6f683fcb619d363b1f26d5325
hoangadrop4-blip <hoangadrop4@gmail.com>
2026-05-08 01:42:49 +0700

    Sync latest IPA source

--- lib/views/utilities/daily_challenge_screen.dart
+++ lib/views/utilities/daily_challenge_screen.dart
@@ -200,7 +200,7 @@ class _DailyChallengeScreenState extends State<DailyChallengeScreen>
                     borderRadius: SLRadius.xlAll,
                     boxShadow: [
                       BoxShadow(
-                          color: Colors.black.withOpacity(0.1),
+                          color: Colors.black.withValues(alpha: 0.1),
                           blurRadius: 20,
                           offset: const Offset(0, 10))
                     ],
