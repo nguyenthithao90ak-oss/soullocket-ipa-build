e6cf7909c89df9ea61ce4d8c6ff55217c8a976cd
hoangadrop4-blip <hoangadrop4@gmail.com>
2026-05-08 07:49:57 +0700

    Sync latest IPA source

--- lib/views/home/tabs/settings/settings_theme_section.dart
+++ lib/views/home/tabs/settings/settings_theme_section.dart
@@ -123,11 +123,6 @@ extension _SettingsTabThemeSection on _SettingsTabState {
       return;
     }
 
-    if (Platform.isIOS) {
-      _showToast('Tính năng PRO tạm ẩn trên iOS.', success: false);
-      return;
-    }
-
     await Navigator.push(
       context,
       MaterialPageRoute(
